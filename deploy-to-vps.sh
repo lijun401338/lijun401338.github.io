4bc91639f4efbf6bcda14f75ddb5bd49b016e15f
8427003 <8427003@qq.com>
2016-09-20 16:25:48 +0000

    rebuild pages at 4901fb9

--- deploy-to-vps.sh
+++ deploy-to-vps.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-rm -rf ./www && git clone https://github.com/lijun401338/lijun401338.github.io.git www
+
+rm -rf ./www-bk && git clone https://github.com/lijun401338/lijun401338.github.io.git www-bk
+
+rm -rf ./www && mv www-bk www
